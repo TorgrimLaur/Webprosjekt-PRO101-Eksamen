36c5893d163328bd28e3fb3dcb114b544990ef82
Karsten Sandhaaland <karsten.sand@haugnett.no>
2018-05-26 11:47:34 +0200

    26 mai start

--- online/wp/wp-content/themes/page-builder-framework/js/script.js
+++ online/wp/wp-content/themes/page-builder-framework/js/script.js
@@ -1,23 +1,36 @@
+/*
+Script to give the "finn veien" site functionality where elements
+swtich between hide/show state based on button clicks
+*/
+
 jQuery(function($){
-  $('#map_image_box').hide();
+  //$('#map_image_box').hide();
   $('#distance_fjerdingen_vulkan').hide();
   $('#distance_fjerdingen_kvadraturen').hide();
   $('#distance_vulkan_kvadraturen').hide();
 
+  // buttons
+  $('#btn_more_info_fjerdingen_vulkan').hide();
+  $('#btn_more_info_fjerdingen_kvadraturen').hide();
+  $('#btn_more_info_vulkan_kvadraturen').hide();
+
+// text box
+  $('#te_more_information').hide();
 });
 
 jQuery(function($){
   $('#btn_fjerdingen_vulkan').click(function(){
-    $('#map_image_box').attr('src','http://localhost/wordpress/wp-content/uploads/2018/05/Vulkan-Fjerdingen.png');
+    $('#map_image_box').attr('src','https://tek.westerdals.no/~sankar17/wp/wp-content/uploads/2018/05/fjerd-brenn-vulk.png');
 
     $('#distance_fjerdingen_kvadraturen').hide();
     $('#distance_vulkan_kvadraturen').hide();
 
     $('#distance_fjerdingen_vulkan').show();
+    $('#btn_more_info_fjerdingen_vulkan').show();
     });
 
   $('#btn_fjerdingen_kvadraturen').click(function(){
-    $("#test").attr('src','http://localhost/wordpress/wp-content/uploads/2018/05/Kvadraturen-Fjerdingen.png');
+    $("#map_image_box").attr('src','https://tek.westerdals.no/~sankar17/wp/wp-content/uploads/2018/05/kvad-fjerd.png');
 
     $('#distance_fjerdingen_vulkan').hide();
     $('#distance_vulkan_kvadraturen').hide();
@@ -26,7 +39,7 @@ jQuery(function($){
   });
 
   $('#btn_vulkan_kvadraturen').click(function(){
-    $("#test").attr('src','http://localhost/wordpress/wp-content/uploads/2018/05/Kvadraturen-Fjerdingen.png');
+    $("#map_image_box").attr('src','https://tek.westerdals.no/~sankar17/wp/wp-content/uploads/2018/05/kvad-brenn-vulk.png');
 
     $('#distance_fjerdingen_vulkan').hide();
     $('#distance_fjerdingen_kvadraturen').hide();
@@ -34,4 +47,11 @@ jQuery(function($){
     $('#distance_vulkan_kvadraturen').show();
   });
 
+});
+
+jQuery(function($){
+  $('#btn_more_info_fjerdingen_vulkan').click(function(){
+    $('#te_more_information').text("For å komme seg mellom Fjerdingen og vulkan er det lettest å gå langs akerselva. Men man kan også ta bus.");
+    $('#te_more_information').toggle();
+  });
 });
